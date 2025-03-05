9a6b894190e3e28156e4060e49f20ce70d551790
danialmousavi <danialmoosavi69@gmail.com>
2025-03-05 19:18:34 +0330

    get podcasts from firebase

--- src/components/podcasts/MainPodcast.jsx
+++ src/components/podcasts/MainPodcast.jsx
@@ -1,21 +1,32 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import podcastsData from './podcastsData';
 import { Container, Row } from 'react-bootstrap';
 
 export default function MainPodcast() {
-    const [podcasts,setPodcasts]=useState(podcastsData);
+    useEffect(()=>{
+        fetch('https://academy-d62cf-default-rtdb.firebaseio.com/podcastsData.json').then(res=>res.json()).then(data=>setPodcasts(data))
+    },[])
+    const [podcasts,setPodcasts]=useState([]);
     const params=useParams();
     console.log(params);
-    const MainPodcast=podcasts.find(podcast=>podcast.id==params.Id);
+    const MainPodcast=[];
+    MainPodcast.push(podcasts.find(podcast=>podcast.id==params.Id));
     console.log(MainPodcast);
   return (
         <>
         <Container>
             <Row>
-                <h1 className='text-warning'>{MainPodcast.title}</h1>
-                <img src={MainPodcast.img} className='img-fluid' style={{height:'50vh'}} alt="" />
-                <p className='text-white fs-4' style={{lineHeight:'2'}}>{MainPodcast.desc}</p>
+          {MainPodcast.length > 0 && MainPodcast[0] ? (
+            <>
+                <img className='img-fluid rounded-5' src={MainPodcast[0].img} alt="" />
+                <h1 className='text-warning mt-3 '>{MainPodcast[0].title}</h1>
+                <p className='text-white mt-3 fs-5' style={{lineHeight:'50px'}}>{MainPodcast[0].desc}</p>
+            </>
+          ) : (
+            <div className='text-white ' style={{height:'100vh'}}><h1>loading...</h1></div>
+
+          )}
             </Row>
         </Container>
         </>
--- src/components/podcasts/Podcasts.jsx
+++ src/components/podcasts/Podcasts.jsx
@@ -1,19 +1,24 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import podcastsData from './podcastsData';
 import CourseCard from '../CourseCard/MainCard';
 
 export default function Podcasts() {
-    const [podcasts,setPodcasts]=useState(podcastsData);
+    const [podcasts,setPodcasts]=useState([]);
+    useEffect(()=>{
+    fetch('https://academy-d62cf-default-rtdb.firebaseio.com/podcastsData.json').then(res=>res.json()).then(data=>setPodcasts(data))
+    },[])
   return (
-    <Container>
-    <Row>
+    <Container style={{backgroundColor:'#1D1E1F'}}>
+      {podcasts.length > 0 ? (<>
+        <Row>
       {podcasts.map(podcast=>(
         <Col key={podcast.id} xs={12} sm={6} md={4} className='g-2'>
           <CourseCard {...podcast}/>
         </Col>
       ))}
     </Row>
+      </>):(<div className='text-white ' style={{height:'100vh'}}><h1>loading...</h1></div>)}
   </Container>
   )
 }
