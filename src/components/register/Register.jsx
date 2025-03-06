58975fcb7f1c9b311c6f555a9f613bd0c863e20d
danialmousavi <danialmoosavi69@gmail.com>
2025-03-06 13:11:20 +0330

    add sweet alert

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Container, Row } from 'react-bootstrap'
+import Swal from 'sweetalert2';
 
 export default function Register() {
     const [userName,setUserName]=useState('');
@@ -19,7 +20,18 @@ export default function Register() {
                 method:'POST',
                 body:JSON.stringify(newUser)
             })
-            .then(res=>res.json())
+            .then(res=>{
+                res.json();
+                if(res.status==200){
+                    Swal.fire({
+                        title: "congratulations",
+                        text: "you have register successfuly",
+                        icon: "success"
+                      });
+                    
+                }
+                
+            })
             .then(data=>console.log(data)
             )
             setUserName('');
@@ -27,18 +39,18 @@ export default function Register() {
             setEmail('');
         }
     }
-  return (
+return (
     <>
-        <Container style={{backgroundColor:'#1D1E1F',width:'100vh',height:'100vh'}} >
-            <Row>
-                <form onSubmit={handleSubmit} className=' p-4 rounded-5 mt-5 d-flex flex-column justify-content-center align-items-center' style={{height:'50vh',backgroundColor:'#303131'}}>
-                    <input onChange={(e)=>setUserName(e.target.value)} value={userName} type="text" className='form-control mt-3 p-3' placeholder='User Name' />
-                    <input onChange={(e)=>setPassWord(e.target.value)} value={passWord} type="password" className='form-control mt-3 p-3' placeholder='Password' />
-                    <input onChange={(e)=>setEmail  (e.target.value)} value={email} type="email" className='form-control mt-3 p-3' placeholder='Email' />
-                    <button type='submit' className='mt-4 btn btn-warning w-100'>submit</button>
-                </form>
-            </Row>
-        </Container>
+            <Container fluid className="d-flex justify-content-center align-items-center" style={{backgroundColor:'#1D1E1F', minHeight:'100vh'}} >
+                    <Row className="w-100">
+                            <form onSubmit={handleSubmit} className='p-4 rounded-5 mt-5 d-flex flex-column justify-content-center align-items-center mx-auto' style={{maxWidth:'400px', width:'100%', backgroundColor:'#303131'}}>
+                                    <input onChange={(e)=>setUserName(e.target.value)} value={userName} type="text" className='form-control mt-3 p-3' placeholder='User Name' />
+                                    <input onChange={(e)=>setPassWord(e.target.value)} value={passWord} type="password" className='form-control mt-3 p-3' placeholder='Password' />
+                                    <input onChange={(e)=>setEmail(e.target.value)} value={email} type="email" className='form-control mt-3 p-3' placeholder='Email' />
+                                    <button type='submit' className='mt-4 btn btn-warning w-100'>Submit</button>
+                            </form>
+                    </Row>
+            </Container>
     </>
 )
 }
